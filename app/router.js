7d81a9377108283fdea58bc3971257458a7bd1a3
aabiro <aabiro@users.noreply.github.com>
2017-11-10 14:20:48 -0500

    added final ember app completed

--- app/router.js
+++ app/router.js
@@ -7,6 +7,12 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('about');
+  this.route('home');
+  this.route('contact', function() {
+    this.route('phone');
+    this.route('email');
+  });
 });
 
 export default Router;
